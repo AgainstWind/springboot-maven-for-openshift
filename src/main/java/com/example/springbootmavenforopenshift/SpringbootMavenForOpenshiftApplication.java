1b1d010f6960da25e644b32fbb5a17ecca3b4968
yuezht <75134974@qq.com>
2018-02-07 16:41:13 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootmavenforopenshift/SpringbootMavenForOpenshiftApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootmavenforopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMavenForOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMavenForOpenshiftApplication.class, args);
+	}
+}
